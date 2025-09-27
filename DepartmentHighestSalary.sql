05eed92ed2396f9a315aba50aaa34b4b6a5456cf
Shridhar Bhandar <shridhar2bhandar@gmail.com>
2025-09-27 23:24:18 +0530

    Create DepartmentHighestSalary.sql

---
+++ DepartmentHighestSalary.sql
@@ -0,0 +1,17 @@
+WITH top_salary AS (
+    SELECT 
+        name, 
+        departmentId, 
+        salary,
+        RANK() OVER (PARTITION BY departmentId ORDER BY salary DESC) AS RankSalary
+    FROM Employee
+)
+SELECT 
+    d.name AS Department,
+    e.name AS Employee,
+    e.salary AS Salary
+FROM top_salary e
+JOIN Department d 
+    ON e.departmentId = d.id
+WHERE e.RankSalary = 1
+ORDER BY Salary DESC;
